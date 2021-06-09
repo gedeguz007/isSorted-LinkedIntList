ab6dbc3a58f89b40d13e4100f866759ca086041f
Gabe DeGuzman <72414796+gedeguz007@users.noreply.github.com>
2021-06-08 20:30:57 -0700

    Create ListTest.java

---
+++ ListTest.java
@@ -0,0 +1,25 @@
+// Class to test methods of LinkedIntList class
+
+public class ListTest {
+	
+	public static void main(String[] args) {
+	     
+
+	     LinkedIntList list = new LinkedIntList();
+	     processList(list);
+	}
+
+	 public static void processList(LinkedIntList list) {
+	     list.add(20);
+	     list.add(11);
+	     list.add(73);
+	     list.add(111);
+	     System.out.println(list);
+	     //System.out.println(list.toString());
+	     list.remove(1);
+	     System.out.println(list);
+	     //System.out.println(list.size());
+	     System.out.print("List sorted?" + " ");
+	     System.out.println(list.isSorted()); 	        
+	 }
+}
